d96811da7cd45139f4847d2c306965a7e126319d
Matthewsamuels20 <78520602+Matthewsamuels20@users.noreply.github.com>
2025-02-28 19:36:45 -0500

    Footer and Home Page Completed

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -16,7 +16,19 @@ const Footer = () => {
       </div>
       <ul>
         <li>Audio Description</li>
+        <li>Help Center</li>
+        <li>Gift Cards</li>
+        <li>Media Centre</li>
+        <li>Investor Relations</li>
+        <li>Jobs</li>
+        <li>Terms of Use</li>
+        <li>Privacy</li>
+        <li>Legal Notices</li>
+        <li>Cookie Preferences</li>
+        <li>Corporate Information</li>
+        <li>Contact Us</li>
       </ul>
+      <p className="copyright-text">&copy; 1997-2025 Netflix, Inc</p>
     </div>
   );
 };
